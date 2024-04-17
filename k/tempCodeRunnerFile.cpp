54a0a8c704fa805b1b81583a178edc2da7c23fb6
Abela012 <137502677+Abela012@users.noreply.github.com>
2024-04-17 14:28:03 +0300

    Add files via upload

---
+++ k/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+string abel[]={'A','b','e','l'}
+for(i=0;i<=len(abel);i++)
+cout<<i*abel
---
+++ k/tempCodeRunnerFile.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class TestJavaCollection9{
+public static void main(String args[]){
+//Creating and adding elements  
+TreeSet<Integer> set=new TreeSet<>();
+set.add(5);
+set.add(2);
+set.add(1);
+set.add(3);
+set.add(4);
+//traversing elements  
+Iterator<String> itr=set.iterator();
+while(itr.hasNext()){
+System.out.println(itr.next());
+}
+}
+}
